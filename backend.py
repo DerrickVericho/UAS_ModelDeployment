023be9611f364fc35a41813bad3f6f8364713e2c
DerrickVericho <derrickvericho@gmail.com>
2025-06-19 20:39:53 +0700

    init: add backend

---
+++ backend.py
@@ -0,0 +1,101 @@
+from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+import pickle
+import pandas as pd
+from starlette import status
+import numpy as np
+
+app = FastAPI()
+
+# Model setting
+model = "model_best.pkl"
+target_encoder = "target_encoder.pkl"
+preprocessor = "preprocessor.pkl"
+
+# Class input
+class Input(BaseModel):
+    Gender: str
+    Age: float             
+    Height: float
+    Weight: float
+    family_history_with_overweight: str
+    FAVC: str
+    FCVC: float
+    NCP: float
+    CAEC: str
+    SMOKE: str
+    CH2O: float
+    SCC: str
+    FAF: float
+    TUE: float
+    CALC: str
+    MTRANS: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "Gender": "Male",
+                "Age": 23,
+                "Height": 1.75,
+                "Weight": 70.0,
+                "family_history_with_overweight": "yes",
+                "FAVC": "no",
+                "FCVC": 2.0,
+                "NCP": 3.0,
+                "CAEC": "Sometimes",
+                "SMOKE": "no",
+                "CH2O": 3.0,
+                "SCC": "yes",
+                "FAF": 1.0,
+                "TUE": 2.0,
+                "CALC": "no",
+                "MTRANS": "Public_Transportation"
+            }
+        }
+
+# Class prediksi   
+class Prediction(BaseModel):
+    prediction: str
+    probability: float
+    
+# Load model pickle
+with open(model, 'rb') as f:
+    model = pickle.load(f)
+  
+with open(target_encoder, 'rb') as f:
+    target_encoder = pickle.load(f)
+    
+with open(preprocessor, 'rb') as f:
+    preprocessor = pickle.load(f)
+      
+
+@app.post("/predict", status_code=status.HTTP_200_OK)
+async def predict(input: Input):
+    try:
+        data = pd.DataFrame([input.dict()])
+        
+        # Map binary categorical
+        gender_map = {'Male': 1, 'Female': 0}
+        boolean_map = {'yes': 1, 'no': 0}
+        
+        # Encode data input & preprocess
+        data['Gender'] = data['Gender'].map(gender_map)
+        data['family_history_with_overweight'] = data['family_history_with_overweight'].map(boolean_map)
+        data['FAVC'] = data['FAVC'].map(boolean_map)
+        data['SMOKE'] = data['SMOKE'].map(boolean_map)
+        data['SCC'] = data['SCC'].map(boolean_map)
+        encoded = preprocessor.transform(data)
+        
+        # Prediction
+        prediction_num = model.predict(encoded)
+        prediction = target_encoder.inverse_transform(prediction_num)[0] # Convert to string prediction
+        proba = model.predict_proba(encoded)
+        max_proba = np.max(proba, axis=1)[0] # Hitung probabilitasnya
+
+        return Prediction(prediction=prediction, probability=max_proba)
+    
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(e)
+        )
